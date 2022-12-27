3041eb78948a25abd72ea7f17bdcac7562a2eea4
Artem Vasilev <kern.usr@gmail.com>
2022-12-27 21:02:35 +0300

    Класс DetailedAccountReport

--- src/Type/DetailedAccountReport.php
+++ src/Type/DetailedAccountReport.php
@@ -4,10 +4,16 @@ namespace Webmasterskaya\Soap\CCCB\Type;
 
 use Webmasterskaya\Soap\Base\Type\RequestInterface;
 
+/**
+ * Метод WEB-сервиса возвращает информацию о принятых отправлениях договора за последние 3 месяца.
+ * Информация об отправлениях включает в себя сведения о Заявке, по которой принято отправление,
+ * месте сбора и назначения и сумму доставки отправления. На данный момент информация о сумме доставки
+ * отправления не заполняется (сумма без НДС, Сумма НДС и Сумма с НДС равны 0).
+ */
 class DetailedAccountReport implements RequestInterface
 {
     /**
-     * @var string
+     * @var string Идентификатор договора контрагента
      */
     private $ContractGUID;
 
@@ -16,7 +22,7 @@ class DetailedAccountReport implements RequestInterface
      *
      * @var string $ContractGUID
      */
-    public function __construct($ContractGUID)
+    public function __construct(string $ContractGUID)
     {
         $this->ContractGUID = $ContractGUID;
     }
@@ -24,7 +30,7 @@ class DetailedAccountReport implements RequestInterface
     /**
      * @return string
      */
-    public function getContractGUID()
+    public function getContractGUID(): string
     {
         return $this->ContractGUID;
     }
@@ -33,7 +39,7 @@ class DetailedAccountReport implements RequestInterface
      * @param string $ContractGUID
      * @return DetailedAccountReport
      */
-    public function withContractGUID($ContractGUID)
+    public function withContractGUID(string $ContractGUID): DetailedAccountReport
     {
         $new = clone $this;
         $new->ContractGUID = $ContractGUID;
