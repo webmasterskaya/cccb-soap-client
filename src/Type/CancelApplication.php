fe45947521c7e2d4abe3802a6cc0afb6510f7bcc
Artem Vasilev <kern.usr@gmail.com>
2022-12-27 21:09:41 +0300

    Класс CancelApplication

--- src/Type/CancelApplication.php
+++ src/Type/CancelApplication.php
@@ -4,6 +4,18 @@ namespace Webmasterskaya\Soap\CCCB\Type;
 
 use Webmasterskaya\Soap\Base\Type\RequestInterface;
 
+/**
+ * <p>Метод WEB-сервиса необходим для отмены заявки в ЕИС. В случае, если в ЕИС по уникальному идентификатору не удалось найти документа
+ * «Заявка на оказание услуг», возвращается ошибка «Не найдена заявка».</p>
+ * <p>Нельзя отменить:</p>
+ * <ul>
+ * <li>заявки, дата сбора которых раньше текущей даты. При попытке отмены такой заявки возвращается ошибка «Нельзя отменить заявку. Заявка уже на исполнении»;</li>
+ * <li>не проведенные заявки. Возвращается ошибка «Документ не проведен. Отмена невозможна.»;</li>
+ * <li>псевдо-заявки. Возвращается ошибка «Нельзя отменить псевдо заявку.»;</li>
+ * <li>скорректированные заявки. Возвращается ошибка «Для заявки <Найденная заявка> уже введены корректировочные заявки»;</li>
+ * <li>заявки, для которых уже осуществлен прием. Возвращается ошибка «Для заявки <Найденная заявка> уже введены приемы».</li>
+ * </ul>
+ */
 class CancelApplication implements RequestInterface
 {
     /**
@@ -16,7 +28,7 @@ class CancelApplication implements RequestInterface
      *
      * @var string $ApplicationGUID
      */
-    public function __construct($ApplicationGUID)
+    public function __construct(string $ApplicationGUID)
     {
         $this->ApplicationGUID = $ApplicationGUID;
     }
@@ -24,7 +36,7 @@ class CancelApplication implements RequestInterface
     /**
      * @return string
      */
-    public function getApplicationGUID()
+    public function getApplicationGUID(): string
     {
         return $this->ApplicationGUID;
     }
@@ -33,7 +45,7 @@ class CancelApplication implements RequestInterface
      * @param string $ApplicationGUID
      * @return CancelApplication
      */
-    public function withApplicationGUID($ApplicationGUID)
+    public function withApplicationGUID(string $ApplicationGUID): CancelApplication
     {
         $new = clone $this;
         $new->ApplicationGUID = $ApplicationGUID;
