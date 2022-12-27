31347d847ec4b7595aadbf4778ca744c8448992e
Artem Vasilev <kern.usr@gmail.com>
2022-12-27 21:43:17 +0300

    Класс GetF1Info

--- src/Type/GetF1Info.php
+++ src/Type/GetF1Info.php
@@ -4,20 +4,27 @@ namespace Webmasterskaya\Soap\CCCB\Type;
 
 use Webmasterskaya\Soap\Base\Type\RequestInterface;
 
+/**
+ * Метод WEB-сервиса возвращает информацию по отправлениям из реестра Ф1.
+ * Состав реестра Ф1 хранится в регистре сведений «Состав реестров Ф1».
+ * Если Реестр Ф1 принимался по договору, отличному от указанного, то возвращается пустое значение.
+ * Статусы отправления, возвращаемые в свойстве «ParcelStatus» структуры ResultOfF1Info
+ * могут принимать следующие значения: принято, в пути, вручено, задержано.
+ */
 class GetF1Info implements RequestInterface
 {
     /**
-     * @var string
+     * @var string Номер реестра Ф1
      */
     private $Number;
 
     /**
-     * @var \DateTimeInterface
+     * @var \DateTimeInterface Дата реестра Ф1
      */
     private $Date;
 
     /**
-     * @var string
+     * @var string Уникальный идентификатор договора, по которому принимался реестр Ф1
      */
     private $ContractGUID;
 
@@ -28,7 +35,7 @@ class GetF1Info implements RequestInterface
      * @var \DateTimeInterface $Date
      * @var string $ContractGUID
      */
-    public function __construct($Number, $Date, $ContractGUID)
+    public function __construct(string $Number, \DateTimeInterface $Date, string $ContractGUID)
     {
         $this->Number = $Number;
         $this->Date = $Date;
@@ -38,7 +45,7 @@ class GetF1Info implements RequestInterface
     /**
      * @return string
      */
-    public function getNumber()
+    public function getNumber(): string
     {
         return $this->Number;
     }
@@ -47,7 +54,7 @@ class GetF1Info implements RequestInterface
      * @param string $Number
      * @return GetF1Info
      */
-    public function withNumber($Number)
+    public function withNumber(string $Number): GetF1Info
     {
         $new = clone $this;
         $new->Number = $Number;
@@ -58,7 +65,7 @@ class GetF1Info implements RequestInterface
     /**
      * @return \DateTimeInterface
      */
-    public function getDate()
+    public function getDate(): \DateTimeInterface
     {
         return $this->Date;
     }
@@ -67,7 +74,7 @@ class GetF1Info implements RequestInterface
      * @param \DateTimeInterface $Date
      * @return GetF1Info
      */
-    public function withDate($Date)
+    public function withDate(\DateTimeInterface $Date): GetF1Info
     {
         $new = clone $this;
         $new->Date = $Date;
@@ -78,7 +85,7 @@ class GetF1Info implements RequestInterface
     /**
      * @return string
      */
-    public function getContractGUID()
+    public function getContractGUID(): string
     {
         return $this->ContractGUID;
     }
@@ -87,7 +94,7 @@ class GetF1Info implements RequestInterface
      * @param string $ContractGUID
      * @return GetF1Info
      */
-    public function withContractGUID($ContractGUID)
+    public function withContractGUID(string $ContractGUID): GetF1Info
     {
         $new = clone $this;
         $new->ContractGUID = $ContractGUID;
